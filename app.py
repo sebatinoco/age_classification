651bf22ea38aeb01767c93ad86766383544ab135
seba <stinoco@fen.uchile.cl>
2021-04-10 12:13:43 -0400

    made major updates to app

--- app.py
+++ app.py
@@ -1,37 +1,60 @@
+import os
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template, flash, redirect
+from werkzeug.utils import secure_filename
+from keras.preprocessing import image
 import pickle
+from keras.models import load_model
+
+UPLOAD_FOLDER = 'store_folder'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+model = load_model('model/age_model.h5')
+with open('model/labels.pkl', 'rb') as handle:
+    labels = pickle.load(handle)
+with open('model/age_dict.pkl', 'rb') as handle:
+    age_dict = pickle.load(handle)
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods = ['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            test_image = image.load_img(f'store_folder/{filename}', target_size = (64, 64))
+            test_image = image.img_to_array(test_image)
+            test_image = np.expand_dims(test_image, axis = 0)
+            result = model.predict(test_image)
+
+            position_result = np.argmax(result)
+            label_result = list(labels.keys())[position_result]
+            prob_result = float(result[0][position_result])
+
+            position_result = np.argmax(result)
+            label_result = list(labels.keys())[position_result]
+            prob_result = float(result[0][position_result])
+            response_text = f'La persona posee {age_dict[label_result]} con un {(prob_result*100):.2f}% de probabilidad!'
+            print(f'store_folder/{filename}')
+            return render_template('index.html', prediction_text = response_text, user_image = f'store_folder/{filename}')
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
-
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-
-    output = prediction[0]
-    return jsonify(output)
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = True)
